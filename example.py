735dface2a15b1afecdccf12ed6e2a869812f32e
AL THE BOT FATHER <98119917+AL-THE-BOT-FATHER@users.noreply.github.com>
2025-06-11 18:24:20 +0200

    Update example.py

--- example.py
+++ example.py
@@ -4,15 +4,16 @@ if __name__ == "__main__":
     rpc_url = ""
     sender_priv_base58_str = ""
     receiver_pubkey_str = ""
-    sol_amount = 0.10
+    sol_amount = 0.1
 
-    hop_transfer = HopTransfer(rpc_url, sender_priv_base58_str, receiver_pubkey_str)
+    hop_transfer = HopTransfer(rpc_url, sender_priv_base58_str, receiver_pubkey_str, sol_amount)
 
     try:
-        results = hop_transfer.execute(sol_amount)
-        print("\n=== Transaction Links ===")
-        print("1) to_hop:", results["to_hop"])
-        print("2) to_receiver:", results["to_receiver"])
-        print("3) recover_hop:", results["recover_hop"])
+        complete = hop_transfer.execute()
+        if complete: 
+            print("Hop transfer complete.")
+        else:
+            print("Hop transfer failed.")
+        
     except Exception as e:
         print("❌ Error during hoped transfer:", e)
